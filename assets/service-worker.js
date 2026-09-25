84893bfc80e0ab36c2db4e9261d9de51586009a3
Jame Roy <jrroy.nh@gmail.com>
2026-09-25 12:55:03 -0400

    Clear Price v1.2.0

---
+++ assets/service-worker.js
@@ -0,0 +1,48 @@
+var CACHE_NAME = 'clear-price-v2';
+var APP_FILES = [
+  './app.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './noto-sans-regular.ttf',
+  './noto-sans-bold.ttf'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+    return cache.addAll(APP_FILES);
+  }).then(function () {
+    return self.skipWaiting();
+  }));
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (key) {
+      return key !== CACHE_NAME;
+    }).map(function (key) {
+      return caches.delete(key);
+    }));
+  }).then(function () {
+    return self.clients.claim();
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(function (cached) {
+    if (cached) return cached;
+    return fetch(event.request).then(function (response) {
+      if (!response || response.status !== 200) return response;
+      var copy = response.clone();
+      caches.open(CACHE_NAME).then(function (cache) {
+        cache.put(event.request, copy);
+      });
+      return response;
+    }).catch(function () {
+      if (event.request.mode === 'navigate') return caches.match('./app.html');
+      return Response.error();
+    });
+  }));
+});
